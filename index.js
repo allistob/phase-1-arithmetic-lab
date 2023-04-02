89c5531b967e0c1add93dbf45b3c2e8e27dec93b
Allison Tobar <allisontobarrodriguez@gmail.com>
2023-04-02 01:53:14 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2
 
-// Write your code here
+let random = 17;
+
+let num3 = 24
+let num4 = 6
+let mod = (num3 / num4) 
+
+const max = Math.max(3,9,20,6,7,15)
